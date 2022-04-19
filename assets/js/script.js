f930d27d31ff57fcd4d30f7fb6736e66ed1e53d5
clickity-clacking <130marissa.martinez@gmail.com>
2022-04-18 21:02:43 -0700

    details of styling and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,20 @@
-//variables
+//Retrieve from storage
+var prefs = JSON.parse(localStorage.getItem("prefs")) || [];
 
-//open to main page containing food selector generator that can function right away
-    //function retrieve data from storage if present
 
-    //event listener for button press to provide food option
+//Function: generate and display recs
+var displayRecs = function(){
+    var recs = [];
+    //populate recs
 
-        //if food option clicked
+    //display recs on page
+    $(".bg-blue").each(function(index){
+        $(this).append("<h3 class='rec'>TEST</h3>")
+        $(".rec").last().val(recs[i])
+    });
+};
 
-            //function display restaurant info
-        
-        //else 
-
-            //remove first suggestion from restaurant list 
-
-            //reset so they can press button again and get new food option
-//
-
-//an option from the main page takes you to a survey where you can fill out food prefs
-
-    //check list where you can select multiple items to select cuisine types
-
-    //check list where you can select multiple items to select price bracket
-
-    //more preference options to be added based on readily available yelp/food review site API attributes
-
-    //submit button
-
-        //filter base restaurant array to match criteria from form
-
-        //store in local storage
+//Function: save to storage
+var savePrefs = function(prefs){
+    localStorage.setItem("prefs", JSON.stringify(prefs));
+};
